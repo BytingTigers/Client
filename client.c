df6ecd0b61041504beb8bdcb30e5386682ed6eeb
hokuma <ho7221@gmail.com>
2023-12-01 14:30:49 +0900

    communication encryption

--- client.c
+++ client.c
@@ -36,9 +36,10 @@ int room_cnt = 0;
 void ssl_send(unsigned char *plaintext, int sockfd)
 {
     int plaintext_len = strlen((char*)plaintext), ciphertext_len = 0;
-    unsigned char buffer[BUFFER_SIZE + AES_BLOCK_SIZE + AES_BLOCK_SIZE]; // ciphertext + padding + iv
+    unsigned char buffer[BUFFER_SIZE + AES_BLOCK_SIZE + AES_BLOCK_SIZE + 2]; // ciphertext + padding + iv + length
     unsigned char ciphertext[BUFFER_SIZE + AES_BLOCK_SIZE];
     unsigned char iv[AES_BLOCK_SIZE];
+	unsigned char length_byte[2];
     RAND_bytes(iv, sizeof(iv));
 
     EVP_CIPHER_CTX *ctx;
@@ -55,9 +56,13 @@ void ssl_send(unsigned char *plaintext, int sockfd)
 
     EVP_CIPHER_CTX_free(ctx);
 
+    length_byte[0] = (ciphertext_len >> 8) & 0xff;
+    length_byte[1] = ciphertext_len & 0xff;
+
     memset(buffer, 0, sizeof(buffer));
     memcpy(buffer, ciphertext, ciphertext_len);
     memcpy(buffer + BUFFER_SIZE + AES_BLOCK_SIZE, iv, AES_BLOCK_SIZE);
+	memcpy(buffer + BUFFER_SIZE + AES_BLOCK_SIZE + AES_BLOCK_SIZE, length_byte, 2);
 
     send(sockfd, buffer, sizeof(buffer), NULL);
 }
@@ -66,8 +71,9 @@ void ssl_recv(unsigned char *plaintext, int sockfd)
 {
     unsigned char iv[AES_BLOCK_SIZE];
 	unsigned char plaintext_buffer[BUFFER_SIZE];
-    unsigned char buffer[BUFFER_SIZE + AES_BLOCK_SIZE + AES_BLOCK_SIZE]; // ciphertext + padding + iv
+    unsigned char buffer[BUFFER_SIZE + AES_BLOCK_SIZE + AES_BLOCK_SIZE + 2]; // ciphertext + padding + iv + length
     unsigned char ciphertext[BUFFER_SIZE + AES_BLOCK_SIZE];
+	unsigned char length_byte[2];
 
     int ciphertext_len = 0, plaintext_len = 0;
     int recv_len = recv(sockfd, buffer, sizeof(buffer), 0);
@@ -76,18 +82,11 @@ void ssl_recv(unsigned char *plaintext, int sockfd)
         plaintext = NULL;
         return;
     }
-    ciphertext_len = recv_len - AES_BLOCK_SIZE;
+
+    ciphertext_len = (buffer[BUFFER_SIZE + AES_BLOCK_SIZE + AES_BLOCK_SIZE] << 8) + buffer[BUFFER_SIZE + AES_BLOCK_SIZE + AES_BLOCK_SIZE + 1];
 
     memcpy(iv, buffer + BUFFER_SIZE + AES_BLOCK_SIZE, AES_BLOCK_SIZE);
     memcpy(ciphertext, buffer, BUFFER_SIZE + AES_BLOCK_SIZE);
-
-	// remove following null bytes
-    for(int i=0;i<ciphertext_len;i++){
-        if(ciphertext[i] == 0){
-            ciphertext_len = i;
-            break;
-        }
-    }
 
     EVP_CIPHER_CTX *ctx;
     int len;
@@ -105,7 +104,8 @@ void ssl_recv(unsigned char *plaintext, int sockfd)
     plaintext_buffer[plaintext_len] = '\0';
 
 	memset(plaintext, 0, sizeof(plaintext));
-    strncpy(plaintext, plaintext_buffer, plaintext_len);
+    memcpy(plaintext, plaintext_buffer, plaintext_len);
+    plaintext[plaintext_len] = '\0';
 }
 
 void init_scr(){
